6b0991d3127dcba78bdde5cc7455ac6a1b349d95
Deepak Jain <deepakbaba92@gmail.com>
2026-07-08 01:19:35 +0530

    Add Python print programs

---
+++ 1_Hello.py
@@ -0,0 +1,27 @@
+print("Welcome to the Python Learning from Scratch with Maths Concepts")
+
+# 1. Blank print
+print()
+
+# 2. Number print
+print(1)
+
+# 3. String print
+print("Deepak")
+
+# 4. Negative Number print
+print(-100)
+
+# 5. Special character print
+print("@#$%^&")
+
+# 6. Even Number print
+print(2,4,6,8,10)
+
+# 7. Odd Number print
+print(1,3,5,7,9)
+
+# 8. Escape characters (\n, \t) print
+print("\t")
+
+print("\n")
