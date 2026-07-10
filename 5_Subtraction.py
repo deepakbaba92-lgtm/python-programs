0073935160f709135974e80fa3289c07ee798f92
Deepak Jain <deepakbaba92@gmail.com>
2026-07-11 00:12:42 +0530

    Add subtraction Python Program

---
+++ 5_Subtraction.py
@@ -0,0 +1,41 @@
+# # Two Numbers ka Subtraction
+a = 50
+b = 20
+
+# print(a - b)
+
+# User Input
+a = int(input("Enter First Number: "))
+b = int(input("Enter Second Number: "))
+print("Subtraction =", a - b)
+
+# Formula Format
+a = 80
+b = 35
+print(f"{a} - {b} = {a-b}")
+
+# # Mini Subtraction Calculator
+print("===== Subtraction Calculator =====")
+
+a = int(input("Enter First Number: "))
+b = int(input("Enter Second Number: "))
+result = a - b
+print("Answer =", result)
+
+# Real-life Example
+
+# Bank Balance
+
+balance = int(input("Enter Balance: "))
+withdraw = int(input("Enter Withdraw Amount: "))
+
+remaining = balance - withdraw
+
+print("Remaining Balance =", remaining)
+
+
+# Age Calculator
+current_year = int(input("Enter Balance: "))
+birth_year = int(input("Enter Withdraw Amount: "))
+age = current_year - birth_year
+print("Age =", age)
